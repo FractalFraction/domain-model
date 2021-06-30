af6865558d43072f107b10ed10af3ca6fce139aa
Colin <cln.gardiner@outlook.com>
2021-06-30 13:49:25 +0100

    updated spec file

--- specs/basket-test.spec.js
+++ specs/basket-test.spec.js
@@ -1,5 +1,5 @@
 assertEquals = require("../src/testing-framework.js");
-assertEquals = requrie("../src/basket.js");
+assertEquals = require("../src/basket.js");
 
 // Setup 
 // Check if the basket object is set up
@@ -14,25 +14,21 @@ const itemCapacity = 10;
 const myBasket = new Basket(items,itemCapacity);
 
 // Execute
-myBasket.getCapacity();
+myBasket.capacity;
 // Return Number 
-myBasket.getItems();
+myBasket.items;
 // Return Array
 myBasket.addItem(newItem);
 // Return String 
 myBasket.getItems();
 // Return Array: 
-myBasket.isFull();
-// Return Boolean 
 
 // Verify
-console.log(assertEquals(myBasket.getCapacity(),10))
+console.log(assertEquals(myBasket.capacity,10))
 // Output: true
-console.log(assertEquals(myBasket.getItems(), items))
+console.log(assertEquals(myBasket.items, items))
 // Output: true
 console.log(assertEquals(myBasket.addItem(),`Added ${newItem} to your basket.`))
 // Output: true
 console.log(assertEquals(myBasket.getItems(), ['chocolate', 'banana', 'peanut butter','bread']))
-//Output: true 
-console.log(myBasket.isFull(),'false');
 // Output: true 
